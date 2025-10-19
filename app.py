e6e8696405d4515bcb5ab6ecaa166e872ffc27ba
Manalalyami7 <manalalyami7@gmail.com>
2025-10-19 16:28:14 +0300

    g

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ from openai import OpenAI
 import streamlit as st
 
 api_key = st.secrets["key"]
+import streamlit as st
 from langchain_openai import ChatOpenAI
-from langchain.schema import HumanMessage, AIMessage, SystemMessage
+from langchain_core.messages import HumanMessage, SystemMessage
 
 st.markdown("# ChatGPT-like clone")
 #this is a comment
